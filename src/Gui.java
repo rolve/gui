6bc63ef644fb3ff9075d0fd9f408a20b1338b670
Michael Faes <rolve@trick17.ch>
2017-08-29 17:00:28 +0200

    We can draw stuff

--- src/Gui.java
+++ src/Gui.java
@@ -1,10 +1,15 @@
+import static java.awt.Color.BLACK;
 import static java.awt.Color.WHITE;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 import static javax.swing.SwingUtilities.invokeAndWait;
 import static javax.swing.SwingUtilities.invokeLater;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,6 +19,9 @@ public class Gui {
     public static final Gui instance = new Gui();
     
     private final JFrame frame;
+    
+    private BufferedImage canvas;
+    private BufferedImage snapshot;
     
     private Gui() {
         frame = createFrame();
@@ -32,15 +40,22 @@ public class Gui {
     
     private JFrame createFrame() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(snapshot, 0, 0, null);
+            }
+        };
         panel.setBackground(WHITE);
         frame.setContentPane(panel);
         return frame;
     }
     
     public void open(String title, int width, int height) {
+        canvas = new BufferedImage(width, height, TYPE_INT_RGB);
         run(() -> {
             frame.setTitle(title);
+            frame.setMinimumSize(new Dimension(200, 100));
             frame.getContentPane().setSize(new Dimension(width, height));
             frame.getContentPane().setPreferredSize(new Dimension(width, height));
             frame.pack();
@@ -53,13 +68,36 @@ public class Gui {
         run(() -> closed.set(frame.isVisible()));
         return closed.get();
     }
-
+    
     public void waitUntilClosed() {
-        while(isOpen()) {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {}
-        }
+        while(isOpen())
+            sleep(50);
+    }
+    
+    public void repaint() {
+        AtomicReference<Dimension> size = new AtomicReference<>();
+        run(() -> size.set(frame.getContentPane().getSize()));
+        snapshot = canvas;
+        initializeCanvas(size.get());
+        frame.repaint();
+    }
+    
+    public void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {}
+    }
+    
+    private void initializeCanvas(Dimension size) {
+        canvas = new BufferedImage(size.width, size.height, TYPE_INT_RGB);
+        canvas.getGraphics().setColor(WHITE);
+        canvas.getGraphics().fillRect(0, 0, size.width, size.height);
+    }
+    
+    public void fillRect(int x, int y, int width, int height) {
+        Graphics graphics = canvas.getGraphics();
+        graphics.setColor(BLACK);
+        graphics.fillRect(x, y, width, height);
     }
     
     private void run(Runnable run) {
--- test/Simple.java
+++ test/Simple.java
@@ -1,8 +1,18 @@
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class Simple {
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Gui gui = Gui.instance;
         gui.open("Simple", 500, 300);
-        gui.waitUntilClosed();
+
+        int t = 0;
+        while(gui.isOpen()) {
+            gui.fillRect((int) (sin(t/10.0) * 50 + 50), (int) (cos(t/10.0) * 50 + 50), 20 + t, 20 + t);
+            gui.repaint();
+            gui.sleep(10);
+            t++;
+        }
     }
 }
