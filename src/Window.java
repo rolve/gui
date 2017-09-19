1a33fe1e0825a268eb11648dc51a5df37a29ad52
Michael Faes <rolve@trick17.ch>
2017-09-19 09:34:11 +0200

    Moved Color class into Window file to simplify deployment

--- src/Window.java
+++ src/Window.java
@@ -11,6 +11,8 @@ import static java.awt.geom.AffineTransform.getScaleInstance;
 import static java.awt.image.AffineTransformOp.TYPE_BICUBIC;
 import static java.awt.image.AffineTransformOp.TYPE_NEAREST_NEIGHBOR;
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 import static java.lang.Math.round;
 import static java.util.Collections.newSetFromMap;
 import static java.util.Collections.singletonMap;
@@ -480,5 +482,20 @@ public class Window {
     /** Converts the given number of native pixels to "user space" pixels (for high-DPI displays). */
     private int toUser(int pixels) {
         return pixels / pixelScale;
+    }
+}
+
+class Color {
+    
+    public final int r, g, b;
+    
+    public Color(int r, int g, int b) {
+        this.r = clamp(r);
+        this.g = clamp(g);
+        this.b = clamp(b);
+    }
+    
+    private static int clamp(int raw) {
+        return max(0, min(255, raw));
     }
 }
