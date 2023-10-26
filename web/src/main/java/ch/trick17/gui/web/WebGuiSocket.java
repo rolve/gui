8c5babd633adb68aea4738733367962461c6c128
Michael Faes <rolve@trick17.ch>
2023-10-26 17:48:10 +0200

    Mouse events seem to work reasonably smoothly, at least when running locally

--- web/src/main/java/ch/trick17/gui/web/WebGuiSocket.java
+++ web/src/main/java/ch/trick17/gui/web/WebGuiSocket.java
@@ -5,6 +5,8 @@ import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 
 import java.io.IOException;
 
+import static java.lang.Double.parseDouble;
+
 public class WebGuiSocket extends WebSocketAdapter {
 
     private int x = 400;
@@ -18,37 +20,81 @@ public class WebGuiSocket extends WebSocketAdapter {
     }
 
     @Override
-    public void onWebSocketText(String message) {
-        switch (message) {
-            case "keyDown  ArrowLeft":
-                x -= 10;
+    public void onWebSocketText(String event) {
+        var name = event.substring(0, 8);
+        var args = event.substring(9);
+        switch (name) {
+            case "keyDown ":
+                handleKeyDown(args);
                 break;
-            case "keyDown  ArrowRight":
-                x += 10;
+            case "keyUp   ":
+                handleKeyUp(args);
                 break;
-            case "keyDown  ArrowUp":
-                y -= 10;
+            case "mouseUp ":
+                handleMouseUp(args);
                 break;
-            case "keyDown  ArrowDown":
-                y += 10;
+            case "mouseDwn":
+                handleMouseDown(args);
                 break;
-            case "keyDown  +":
-                size += 10;
-                break;
-            case "keyDown  -":
-                size -= 10;
+            case "mouseMov":
+                var parts = args.split(",");
+                handleMouseMove(parseDouble(parts[0]), parseDouble(parts[1]));
                 break;
             default:
-                System.out.println("Unknown message: " + message);
-                return;
+                System.out.println("Unknown event: " + event);
         }
+    }
+
+    private void handleKeyDown(String key) {
+        switch (key) {
+            case "ArrowLeft":
+                x -= 10;
+                break;
+            case "ArrowRight":
+                x += 10;
+                break;
+            case "ArrowUp":
+                y -= 10;
+                break;
+            case "ArrowDown":
+                y += 10;
+                break;
+            case "+":
+                size += 10;
+                break;
+            case "-":
+                size -= 10;
+                break;
+        }
+        render();
+    }
+
+    private void handleKeyUp(String key) {
+        // nothing to do
+    }
+
+    private void handleMouseMove(String button) {
+        // nothing to do
+    }
+
+    private void handleMouseUp(String button) {
+        // nothing to do
+    }
+
+    private void handleMouseDown(String button) {
+        // nothing to do
+    }
+
+    private void handleMouseMove(double x, double y) {
+        this.x = (int) x;
+        this.y = (int) y;
         render();
     }
 
     private void render() {
         try {
             getSession().getRemote().sendString(
-                    "clear   \n" +
+                    "clear    \n" +
                     "drawRect " + (x - size / 2) + "," + (y - size / 2) + ","
                     + size + "," + size);
         } catch (IOException e) {
