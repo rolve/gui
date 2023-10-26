18e3d879d0d9b7a15d9933cfc153ab018c729ccd
Michael Faes <rolve@trick17.ch>
2023-10-26 17:28:42 +0200

    Rudimentary key input is working

--- web/src/main/java/ch/trick17/gui/web/WebGuiSocket.java
+++ web/src/main/java/ch/trick17/gui/web/WebGuiSocket.java
@@ -4,36 +4,55 @@ import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 
 import java.io.IOException;
-import java.util.Random;
 
 public class WebGuiSocket extends WebSocketAdapter {
+
+    private int x = 400;
+    private int y = 300;
+    private int size = 100;
 
     @Override
     public void onWebSocketConnect(Session session) {
         super.onWebSocketConnect(session);
+        render();
+    }
 
-        var random = new Random();
-        for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(100);
-                session.getRemote().sendString("setColor "
-                                               + random.nextInt(256) + ","
-                                               + random.nextInt(256) + ","
-                                               + random.nextInt(256));
-                var w = random.nextInt(200);
-                var h = random.nextInt(200);
-                var x = random.nextGaussian() * 400 + 400 - w / 2;
-                var y = random.nextGaussian() * 300 + 300 - h / 2;
-                if (random.nextBoolean()) {
-                    session.getRemote().sendString(
-                            "drawRect " + x + "," + y + "," + w + "," + h);
-                } else {
-                    session.getRemote().sendString(
-                            "fillRect " + x + "," + y + "," + w + "," + h);
-                }
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+    @Override
+    public void onWebSocketText(String message) {
+        switch (message) {
+            case "keyDown  ArrowLeft":
+                x -= 10;
+                break;
+            case "keyDown  ArrowRight":
+                x += 10;
+                break;
+            case "keyDown  ArrowUp":
+                y -= 10;
+                break;
+            case "keyDown  ArrowDown":
+                y += 10;
+                break;
+            case "keyDown  +":
+                size += 10;
+                break;
+            case "keyDown  -":
+                size -= 10;
+                break;
+            default:
+                System.out.println("Unknown message: " + message);
+                return;
+        }
+        render();
+    }
+
+    private void render() {
+        try {
+            getSession().getRemote().sendString(
+                    "clear   \n" +
+                    "drawRect " + (x - size / 2) + "," + (y - size / 2) + ","
+                    + size + "," + size);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
