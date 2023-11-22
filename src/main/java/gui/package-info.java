0cf01edf2f59238f53fc80bb344f612a332145b2
Michael Faes <rolve@trick17.ch>
2023-11-22 19:54:55 +0100

    This will definitely warrant a new major version...

--- src/main/java/gui/package-info.java
+++ src/main/java/gui/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains the main class, {@link gui.Gui}, and a simple {@link gui.Color}
+ * Contains the main interface, {@link gui.Gui}, and a simple {@link gui.Color}
  * class.
  */
 package gui;
